67e5057ba63a837ed96f419220a244d3cad0cf82
khargr14 <kay_hargrove@yahoo.com>
2021-01-13 18:36:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+      "name": name,
+      "email": email
+    }
+    let configObj = {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+        "accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+    return fetch("http://localhost:3000/users", configObj)
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(object) {
+              let p = document.createElement('p');
+              p.textContent = object.id;
+              document.body.appendChild(p);
+          })
+          .catch(function(error) {
+              let p = document.createElement('p');
+              p.textContent = error.message;
+              document.body.appendChild(p);
+          });
+        };
