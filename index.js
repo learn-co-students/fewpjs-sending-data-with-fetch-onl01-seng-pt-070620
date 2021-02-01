8134847f89073c034430af8b9af607692ee8d297
mfioran1 <maurice97f@gmail.com>
2021-02-01 10:21:23 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData(name, email){
+    let formData = {
+        name: name,
+        email: email
+   };
+
+   let configObj = {
+    method: "POST",
+    headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData) 
+    };
+        
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(data => document.body.innerHTML = data.id)
+
+    .catch(function(error){
+        document.body.innerHTML = error.message;
+    });
+    
+    
+    }
